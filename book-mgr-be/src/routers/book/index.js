bf4c33b1aacfa28ec5a95a0ea9b2a495fdfe9105
showmaker <1359777852@qq.com>
2023-02-15 23:27:47 +0800

    完成图书列表的相关操作

---
+++ book-mgr-be/src/routers/book/index.js
@@ -0,0 +1,76 @@
+const Router = require('@koa/router');
+const mongoose = require('mongoose');
+const { getBody } = require('../../helpers/utils');
+const Book = mongoose.model('Book');
+
+const router = new Router({
+    prefix: '/book',
+});
+
+router.post('/add', async (ctx) => {
+    const {
+        name,
+        price,
+        author,
+        publishDate,
+        classify,
+    } = getBody(ctx);
+
+    const book = new Book({
+        name,
+        price,
+        author,
+        publishDate,
+        classify,
+    });
+
+    const res = await book.save();
+
+    ctx.body = {
+        data: res,
+        code: 1,
+        msg: '添加成功',
+    };
+});
+
+
+
+router.get('/list', async (ctx) => {
+    const {
+        page = 1,
+        keyword = '',
+    } = ctx.query;
+
+    let = {
+        size = 10,
+    } = ctx.query;
+
+    size = Number(size);
+
+    const query = {};
+
+    if (keyword) {
+        query.name = keyword;
+    }
+
+    const list = await Book
+        .find(query)
+        .skip((page - 1) * size)
+        .limit(size)
+        .exec();
+
+    const total = await Book.countDocuments();
+
+    ctx.body = {
+        data: {
+            total,
+            list,
+            page,
+            size,
+        },
+        code: 1,
+        msg: '获取列表成功',
+    };
+});
+
+module.exports = router;
