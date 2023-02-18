976ae30c34c4f55aafeb9fbdb238ae4b35f354b5
showmaker <1359777852@qq.com>
2023-02-18 22:26:29 +0800

    完成角色管理

--- book-mgr-be/src/routers/index.js
+++ book-mgr-be/src/routers/index.js
@@ -2,10 +2,12 @@ const auth = require('./auth');
 const inviteCode = require('./invite-code');
 const book = require('./book');
 const InventoryLog = require('./inventory-log');
+const user = require('./user');
 
 module.exports = (app) => {
     app.use(auth.routes());
     app.use(inviteCode.routes());
     app.use(book.routes());
     app.use(InventoryLog.routes());
+    app.use(user.routes());
 };
